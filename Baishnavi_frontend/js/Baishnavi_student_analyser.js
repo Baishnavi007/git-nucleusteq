6e7483eca453401c09f3717183e75db108a2c288
Baishnavi007 <s.baishnavi1@gmail.com>
2026-03-19 13:32:23 +0530

    feat: add student data structure using array of objects

---
+++ Baishnavi_frontend/js/Baishnavi_student_analyser.js
@@ -0,0 +1,48 @@
+// Student Performance Analyzer
+
+const students = [
+  {
+    name: "Lalit",
+    marks: [
+      { subject: "Math", score: 78 },
+      { subject: "English", score: 82 },
+      { subject: "Science", score: 74 },
+      { subject: "History", score: 69 },
+      { subject: "Computer", score: 88 }
+    ],
+    attendance: 82
+  },
+  {
+    name: "Rahul",
+    marks: [
+      { subject: "Math", score: 90 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 80 },
+      { subject: "History", score: 76 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+    {
+    name: "Rishi",
+    marks: [
+      { subject: "Math", score: 42 },
+      { subject: "English", score: 80 },
+      { subject: "Science", score: 75 },
+      { subject: "History", score: 77 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+      {
+    name: "Ravi",
+    marks: [
+      { subject: "Math", score: 92 },
+      { subject: "English", score: 80 },
+      { subject: "Science", score: 75 },
+      { subject: "History", score: 77 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 74
+  }
+];
