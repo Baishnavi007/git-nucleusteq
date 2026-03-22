ad7cbf09d6cd867da79b998eacf030d570d8595f
Baishnavi007 <s.baishnavi1@gmail.com>
2026-03-22 08:04:31 +0530

    doc: Added comments for better understanding

--- Baishnavi_frontend/js/Baishnavi_student_analyser.js
+++ Baishnavi_frontend/js/Baishnavi_student_analyser.js
@@ -4,7 +4,7 @@ const students = [
   {
     name: "Lalit",
     marks: [
-      { subject: "Math", score: 78 },
+      { subject: "Math", score: 30 },
       { subject: "English", score: 82 },
       { subject: "Science", score: 74 },
       { subject: "History", score: 69 },
@@ -18,7 +18,7 @@ const students = [
       { subject: "Math", score: 90 },
       { subject: "English", score: 85 },
       { subject: "Science", score: 80 },
-      { subject: "History", score: 76 },
+      { subject: "History", score: 35 },
       { subject: "Computer", score: 92 }
     ],
     attendance: 91
@@ -38,15 +38,38 @@ const students = [
     name: "Ravi",
     marks: [
       { subject: "Math", score: 92 },
-      { subject: "English", score: 80 },
-      { subject: "Science", score: 75 },
-      { subject: "History", score: 77 },
+      { subject: "English", score: 90 },
+      { subject: "Science", score: 95 },
+      { subject: "History", score: 97 },
       { subject: "Computer", score: 92 }
+    ],
+    attendance: 77
+  },
+   {
+    name: "Ramesh",
+    marks: [
+      { subject: "Math", score: 50 },
+      { subject: "English", score: 50 },
+      { subject: "Science", score: 55 },
+      { subject: "History", score: 70 },
+      { subject: "Computer", score: 50 }
+    ],
+    attendance: 77
+  },
+     {
+    name: "John",
+    marks: [
+      { subject: "Math", score: 50 },
+      { subject: "English", score: 50 },
+      { subject: "Science", score: 55 },
+      { subject: "History", score: 70 },
+      { subject: "Computer", score: 50 }
     ],
     attendance: 74
   }
 ];
 
+//Function to calculate total marks of students, it uses a simple for loop for this
 function calculateTotalMarks(student) {
   let total = 0;
 
@@ -57,6 +80,16 @@ function calculateTotalMarks(student) {
   return total;
 }
 
+//  Prints the result of calculateTotalMarks function
+students.forEach(student => {
+  const total = calculateTotalMarks(student);
+  
+
+  console.log(`${student.name} Total Marks: ${total}`);
+ 
+});
+
+//Function to calculate the average marks of each student
 function calculateAverageMarks(student) {
   const total = calculateTotalMarks(student);
   const numberOfSubjects = student.marks.length;
@@ -64,6 +97,16 @@ function calculateAverageMarks(student) {
   return total / numberOfSubjects;
 }
 
+//Prints the result of calculateAverageMarks function
+students.forEach(student => {
+  const total = calculateAverageMarks(student);
+  
+
+  console.log(`${student.name} Average Marks: ${total}`);
+ 
+});
+
+//Functipn to get the highest marks subject-wise
 function getSubjectWiseHighest(students) {
   const highestScores = {};
 
@@ -85,6 +128,51 @@ function getSubjectWiseHighest(students) {
 
   return highestScores;
 }
+
+//Prints the output of getSubjectWiseHighest function which return the highest marks per subject
+const highest = getSubjectWiseHighest(students);
+for (let subject in highest) {
+  console.log(
+    `Highest in ${subject}: ${highest[subject].name} (${highest[subject].score})`
+  );
+}
+
+//Function to calculate the average marks subject wise
+function getSubjectWiseAverage(students) {
+  const subjectData = {};
+
+  for (let i = 0; i < students.length; i++) {
+    const student = students[i];
+
+    for (let j = 0; j < student.marks.length; j++) {
+      const mark = student.marks[j];
+      const subject = mark.subject;
+      const score = mark.score;
+
+      // if the subject is coming for the furst time
+      if (!subjectData[subject]) {
+        subjectData[subject] = {
+          total: 0,
+          count: 0
+        };
+      }
+
+      subjectData[subject].total += score;
+      subjectData[subject].count += 1;
+    }
+  }
+
+  return subjectData;
+}
+
+//to print the output of getSubjectWiseAverage function which will print the average marks subject-wise
+const subjectAvg = getSubjectWiseAverage(students);
+for (let subject in subjectAvg) {
+  const avg = subjectAvg[subject].total / subjectAvg[subject].count;
+  console.log(`Average ${subject} Score: ${avg}`);
+}
+
+//Function to get the class topper(student with the highest marks in the class)
 function getClassTopper(students) {
   let topper = null;
   let highestMarks = 0;
@@ -101,3 +189,36 @@ function getClassTopper(students) {
 
   return { name: topper, marks: highestMarks };
 }
+
+//prints the output of the function getClassTopper which returns class topper's name and respective marks
+const topper = getClassTopper(students);
+console.log(`Class Topper: ${topper.name} with ${topper.marks} marks`);
+
+//logic of the function to get the grades of the student and check if they are fail
+function getStudentGrade(student) {
+  // Check subject fail
+  for (let i = 0; i < student.marks.length; i++) {
+    if (student.marks[i].score <= 40) {
+      return `Fail (Failed in ${student.marks[i].subject})`;
+    }
+  }
+
+  // Check attendance fail
+  if (student.attendance < 75) {
+    return "Fail (Low Attendance)";
+  }
+
+  // If passed, calculate grade
+  const average = calculateAverageMarks(student);
+
+  if (average >= 85) return "A";
+  else if (average >= 70) return "B";
+  else if (average >= 50) return "C";
+  else return "Fail";
+}
+
+//Prints the grade of the student which we have received from the function getStudentGrade
+students.forEach(student => {
+  const grade = getStudentGrade(student);
+  console.log(`${student.name} Grade: ${grade}`);
+});
