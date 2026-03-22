25d771f0c021f8726fa09808f28e75a7ead21ca6
Baishnavi007 <s.baishnavi1@gmail.com>
2026-03-22 14:30:26 +0530

    feat: complete js

--- Baishnavi_frontend/mini_app/product_inventory_dashboard/script.js
+++ Baishnavi_frontend/mini_app/product_inventory_dashboard/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {//
+document.addEventListener("DOMContentLoaded", function () {//runs js after html loads
 
 // Default products
 const defaultProducts = [
