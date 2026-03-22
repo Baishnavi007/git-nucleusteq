630d7ab01ae9ec28b59e27d0f296b524c68f950b
Baishnavi007 <s.baishnavi1@gmail.com>
2026-03-22 13:04:22 +0530

    feat: add default products

---
+++ Baishnavi_frontend/mini_app/product_inventory_dashboard/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {//runs js after html loads
+
+// Default products
+const defaultProducts = [
+    { id: 1, name: "Laptop", price: 55000, stock: 5, category: "electronics" },
+    { id: 2, name: "Phone", price: 20000, stock: 10, category: "electronics" },
+    { id: 3, name: "T-Shirt", price: 500, stock: 20, category: "clothing" },
+    { id: 4, name: "Jeans", price: 1500, stock: 8, category: "clothing" },
+    { id: 5, name: "Book", price: 300, stock: 0, category: "books" },
+    { id: 6, name: "Notebook", price: 100, stock: 15, category: "books" },
+    { id: 7, name: "Headphones", price: 2000, stock: 3, category: "accessories" },
+    { id: 8, name: "Charger", price: 800, stock: 12, category: "accessories" }
+];
+
+// Main product array
+let products = [];
+
+// Filtered products
+let filteredProducts = [];
+
+});
