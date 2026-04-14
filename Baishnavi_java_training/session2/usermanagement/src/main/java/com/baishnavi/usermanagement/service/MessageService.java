dee381c7ff7870d9b6065c8e8537d37fbb48646b
Baishnavi007 <s.baishnavi1@gmail.com>
2026-04-14 23:09:28 +0530

    feat:Updated the MessageService for null messages

--- Baishnavi_java_training/session2/usermanagement/src/main/java/com/baishnavi/usermanagement/service/MessageService.java
+++ Baishnavi_java_training/session2/usermanagement/src/main/java/com/baishnavi/usermanagement/service/MessageService.java
@@ -1,8 +1,11 @@
 package com.baishnavi.usermanagement.service;
+
 import com.baishnavi.usermanagement.component.MessageFormatter;
 import com.baishnavi.usermanagement.exception.InvalidMessageTypeException;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 
 // Service which decides which formatter to use
 @Service
@@ -10,20 +13,28 @@ public class MessageService {
 
     private final Map<String, MessageFormatter> formatterMap;
 
-
-    // Spring injects all formatter components automatically
+    // Constructor Injection
     public MessageService(Map<String, MessageFormatter> formatterMap) {
         this.formatterMap = formatterMap;
     }
 
-    //Get message based on type
+    // Get message based on type
     public String getMessage(String type) {
 
+        // Handle null or empty input
+        if (type == null || type.isBlank()) {
+            throw new InvalidMessageTypeException("null/empty");
+        }
+
+        // Convert input to uppercase to match component keys
         MessageFormatter formatter = formatterMap.get(type.toUpperCase());
+
+        // Handle invalid type
         if (formatter == null) {
             throw new InvalidMessageTypeException(type);
         }
 
+        // Return formatted message
         return formatter.formatMessage();
     }
 }
