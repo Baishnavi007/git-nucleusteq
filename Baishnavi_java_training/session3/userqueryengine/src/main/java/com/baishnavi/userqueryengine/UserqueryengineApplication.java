cdb20e794161f2cbaa2f7cabbdf844230e42b514
Baishnavi007 <s.baishnavi1@gmail.com>
2026-04-15 15:20:15 +0530

    Added session3 folder and project structure

---
+++ Baishnavi_java_training/session3/userqueryengine/src/main/java/com/baishnavi/userqueryengine/UserqueryengineApplication.java
@@ -0,0 +1,13 @@
+package com.baishnavi.userqueryengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserqueryengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserqueryengineApplication.class, args);
+	}
+
+}
---
+++ Baishnavi_java_training/session3/userqueryengine/src/test/java/com/baishnavi/userqueryengine/UserqueryengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baishnavi.userqueryengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserqueryengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
