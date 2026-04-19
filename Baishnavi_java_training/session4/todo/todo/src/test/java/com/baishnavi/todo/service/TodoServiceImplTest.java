d03e9af288f03ec6d7fe80b1b403e169a52aabf6
Baishnavi007 <s.baishnavi1@gmail.com>
2026-04-19 16:59:35 +0530

    test: setup Mockito mocks and inject dependencies for service testing

--- Baishnavi_java_training/session4/todo/todo/src/test/java/com/baishnavi/todo/service/TodoServiceImplTest.java
+++ Baishnavi_java_training/session4/todo/todo/src/test/java/com/baishnavi/todo/service/TodoServiceImplTest.java
@@ -1,6 +1,29 @@
 package com.baishnavi.todo.service;
 
-// This class is used to test TodoServiceImpl methods
- public class TodoServiceImplTest {
+import com.baishnavi.todo.repository.TodoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+// This class is used to test TodoServiceImpl methods
+public class TodoServiceImplTest {
+
+    // Fake repository (real DB call is not happening)
+    @Mock
+    private TodoRepository repository;
+
+    // Mock external service
+    @Mock
+    private NotificationServiceClient notificationServiceClient;
+
+    // Actual service which is being tested(With injected mocks)
+    @InjectMocks
+    private TodoServiceImpl service;
+
+    // Setup method: runs before each test
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
